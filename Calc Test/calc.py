847f87c4b7fedf8752329e4c8271883f35e6e3b2
Michael Snow Carini <65109289+NorthChild@users.noreply.github.com>
2021-07-01 10:56:01 +0100

    Add files via upload

---
+++ Calc Test/calc.py
@@ -0,0 +1,16 @@
+
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError('You cant divide by 0')
+    else:
+        return a / b
---
+++ Calc Test/test_calc.py
@@ -0,0 +1,32 @@
+import unittest
+import calc
+
+
+class TestCalc(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(calc.add(5, 5), 10)
+        self.assertEqual(calc.add(-5, 5), 0)
+        self.assertEqual(calc.add(-5, -5), -10)
+
+    def test_subtract(self):
+        self.assertEqual(calc.subtract(5, 5), 0)
+        self.assertEqual(calc.subtract(-5, 10), -15)
+        self.assertEqual(calc.subtract(-5, -5), 0)
+
+    def test_multiply(self):
+        self.assertEqual(calc.multiply(1, 5), 5)
+        self.assertEqual(calc.multiply(-5, 5), -25)
+        self.assertEqual(calc.multiply(-5, -5), 25)
+
+    def test_divide(self):
+        self.assertEqual(calc.divide(5, 1), 5)
+        self.assertEqual(calc.divide(-5, 10), -0.5)
+        self.assertEqual(calc.divide(-5, -5), 1)
+
+        with self.assertRaises(ValueError):
+            calc.divide(10, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
